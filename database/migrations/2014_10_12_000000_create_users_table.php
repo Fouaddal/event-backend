72daf4b36ca94275150a4b17f82525c2e6d5936b
fouad <fouaddalloul027@gmail.com>
2025-08-16 03:06:45 +0300

    new

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void
+return new class extends Migration
+{
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
@@ -16,14 +17,17 @@ return new class extends Migration {
             $table->enum('type', ['user', 'provider', 'admin'])->default('user');
             $table->enum('provider_type', ['individual', 'company'])->nullable();
             $table->boolean('is_approved')->default(false);
-            $table->rememberToken();
             $table->string('otp')->nullable();
-            $table->json('services')->nullable(); 
+            $table->json('services')->nullable();
+            $table->json('specializations')->nullable();
+            $table->text('description')->nullable();
+            $table->string('profile_image')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
