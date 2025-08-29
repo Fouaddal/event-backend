b7ddbaf253f8c588815a735b46ded09dbde2b895
fouad <fouaddalloul027@gmail.com>
2025-08-30 01:30:31 +0300

    new

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -12,4 +12,10 @@ class Gallery extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function user()
+{
+    return $this->belongsTo(User::class, 'user_id');
+}
+
 }
