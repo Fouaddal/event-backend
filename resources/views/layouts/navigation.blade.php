c5d5e107df2ffdcffc39866656b30835601f51ee
fouad <fouaddalloul027@gmail.com>
2025-08-30 01:32:17 +0300

    new

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,18 +12,22 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <!-- New Dashboard Link -->
+                    <x-nav-link :href="route('admin.new')" :active="request()->routeIs('admin.new')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
+
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('messages.providers') }}
                     </x-nav-link>
 
                     <x-nav-link :href="route('admin.companies')" :active="request()->routeIs('admin.companies')">
-                        {{ __('messages.companies') }}
+                        {{ __('messages.Companies') }}
                     </x-nav-link>
 
                     <x-nav-link :href="route('admin.events.pending')" :active="request()->routeIs('admin.events.pending')">
-    {{ __('Pending Events') }}
-</x-nav-link>
-
+                        {{ __('Events') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -75,8 +79,21 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden" @if(app()->getLocale() == 'ar') dir="rtl" @endif>
         <div class="pt-2 pb-3 space-y-1">
+            <!-- Responsive Dashboard Link -->
+            <x-responsive-nav-link :href="route('admin.new')" :active="request()->routeIs('admin.new')">
+                {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('messages.dashboard') }}
+                {{ __('messages.providers') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('admin.companies')" :active="request()->routeIs('admin.companies')">
+                {{ __('messages.companies') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('admin.events.pending')" :active="request()->routeIs('admin.events.pending')">
+                {{ __('Pending Events') }}
             </x-responsive-nav-link>
         </div>
 
