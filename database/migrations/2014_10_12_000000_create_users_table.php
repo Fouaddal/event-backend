34b6e929515595941e8023d45215a3ef08660fbb
fouad <fouaddalloul027@gmail.com>
2025-08-30 01:42:09 +0300

    new

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('email_confirmation')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('type', ['user', 'provider', 'admin'])->default('user');
